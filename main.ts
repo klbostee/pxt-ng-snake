6dcb729d380717ebf4742d9db7f0aa59cccb307e
Klaas Bosteels <klaas.bosteels@gmail.com>
2020-02-08 18:09:47 +0100

    Initial commit

--- main.ts
+++ main.ts
@@ -1,7 +1,105 @@
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    # . . . #
-    . # # # .
-    `);
+basic.showIcon(IconNames.Snake)
+while (true) {
+    if (input.buttonIsPressed(Button.A) || input.buttonIsPressed(Button.B)) {
+        basic.clearScreen()
+        basic.pause(1000)
+        break
+    }
+}
+
+radio.setGroup(23)
+
+let snake: game.LedSprite[] = [game.createSprite(2, 2)]
+let tail: game.LedSprite[] = []
+
+let food: game.LedSprite = game.createSprite(Math.randomRange(0, 4), Math.randomRange(0, 4))
+food.set(LedSpriteProperty.Blink, 1)
+food.set(LedSpriteProperty.Brightness, 3)
+
+input.onButtonPressed(Button.A, function () {
+    snake[0].turn(Direction.Left, 90)
+})
+input.onButtonPressed(Button.B, function () {
+    snake[0].turn(Direction.Right, 90)
+})
+
+ng.onButtonPressed(ng.NGButtonPin.Up, function () {
+    snake[0].set(LedSpriteProperty.Direction, 0)
+})
+ng.onButtonPressed(ng.NGButtonPin.Right, function () {
+    snake[0].set(LedSpriteProperty.Direction, 90)
+})
+ng.onButtonPressed(ng.NGButtonPin.Down, function () {
+    snake[0].set(LedSpriteProperty.Direction, 180)
+})
+ng.onButtonPressed(ng.NGButtonPin.Left, function () {
+    snake[0].set(LedSpriteProperty.Direction, 270)
+})
+
+kermis.onKermisNotePlayed(function () {
+    ng.neopixels().rotate(1)
+    ng.neopixels().show()
+})
+
+basic.forever(function () {
+    // move snake
+    let prevSprite: game.LedSprite = null
+    let currentSprite: game.LedSprite = null
+    for (let index = 0; index <= snake.length - 2; index++) {
+        currentSprite = snake[snake.length - index - 1]
+        prevSprite = snake[snake.length - index - 2]
+        currentSprite.set(LedSpriteProperty.X, prevSprite.get(LedSpriteProperty.X))
+        currentSprite.set(LedSpriteProperty.Y, prevSprite.get(LedSpriteProperty.Y))
+    }
+    snake[0].move(1)
+    basic.pause(500)
+
+    // check for collisions
+    for (let index = 0; index <= snake.length - 2; index++) {
+        let curSprite = snake[snake.length - index - 1]
+        if (curSprite.isTouching(snake[0])) {
+            game.gameOver()
+        }
+    }
+
+    // eat food
+    if (food.isTouching(snake[0])) {
+        let newTail = game.createSprite(
+            food.get(LedSpriteProperty.X),
+            food.get(LedSpriteProperty.Y)
+        )
+        newTail.set(LedSpriteProperty.Brightness, 5)
+        snake.push(newTail)
+        food.set(LedSpriteProperty.X, Math.randomRange(0, 4))
+        food.set(LedSpriteProperty.Y, Math.randomRange(0, 4))
+        game.addScore(1)
+        radio.sendNumber(game.score())
+        if (game.score() == 10) {
+            // woohoo!
+            game.pause() // pause rendering engine
+            input.onButtonPressed(Button.A, function () {
+                ng.neopixels().showRainbow(1, 360)
+                kermis.playFirstPartOfKermisChorus()
+                ng.neopixels().showColor(neopixel.colors(NeoPixelColors.Black))
+                ng.neopixels().show()
+            })
+            input.onButtonPressed(Button.B, function () {
+                ng.neopixels().showRainbow(1, 360)
+                kermis.playSecondPartOfKermisChorus()
+                ng.neopixels().showColor(neopixel.colors(NeoPixelColors.Black))
+                ng.neopixels().show()
+            })
+            basic.clearScreen()
+            let count = 0;
+            while (true) {
+                count += 1
+                if (count % 2 == 0) {
+                    basic.showIcon(IconNames.SmallHeart)
+                } else {
+                    basic.showIcon(IconNames.Heart)
+                }
+                basic.pause(100);
+            }
+        }
+    }
+})
